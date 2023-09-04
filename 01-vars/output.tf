cf08f1dd150464fc03f6649232baf895399a11ad
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-04 16:26:27 +0530

    terraform code

--- 01-vars/output.tf
+++ 01-vars/output.tf
@@ -20,7 +20,7 @@ output "sample_list_2" {
 
 # Print number1 in sample_dict
 output "sample_dict_num1" {
-  value = var.sample_dict["number1"]jj
+  value = var.sample_dict["number1"]
 }
 
 output "sample_dict_bool" {
